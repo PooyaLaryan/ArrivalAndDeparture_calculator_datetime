3c33715a44563d3a25853f4af949909a4476c5f3
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-10-27 12:33:10 +0330

    Add project files.

---
+++ Samples/Program.cs
@@ -0,0 +1,5 @@
+﻿DateTime dateTime1 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day,9,20,0);
+Console.WriteLine($"arrival time : {dateTime1.ToString("HH:mm")}");
+var dateTime2 = dateTime1.AddHours(9).AddMinutes(15);
+Console.WriteLine($"departure time : {dateTime2.ToString("HH:mm")} -- {dateTime2.ToShortTimeString()} -- {dateTime2.ToString("hh:m tt")}");
+Console.ReadLine();
