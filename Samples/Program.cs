f714fb4093e7e3a13e1161dff01a171e20ac54cf
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-10-27 13:03:51 +0330

    add addday method

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -2,4 +2,15 @@
 Console.WriteLine($"arrival time : {dateTime1.ToString("HH:mm")}");
 var dateTime2 = dateTime1.AddHours(9).AddMinutes(15);
 Console.WriteLine($"departure time : {dateTime2.ToString("HH:mm")} -- {dateTime2.ToShortTimeString()} -- {dateTime2.ToString("hh:m tt")}");
+
+var d1 = DateTime.Now;
+var d2 = DateTime.Now.Date;
+var d3 = DateTime.Now.AddDays(-3).Date;
+var d4 = new DateTime(2024,10, 24, 9, 20, 0).Date;
+
+if (d4 > d3)
+    Console.WriteLine("SQL");
+else
+    Console.WriteLine("ClickHouse");
+
 Console.ReadLine();
